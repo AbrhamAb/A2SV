e211b8f9028c520b7a40c1b8fe21027a3fe020c1
Abrham Abebaw <abrhamabebaw.tech@gmail.com>
2026-01-29 22:39:04 +0300

    Add Python if-else solution for HackerRank problem

---
+++ py_if_else.py
@@ -0,0 +1,16 @@
+# HackerRank Problem: Python If-Else
+# Language: Python
+# Author: Abrham
+
+if __name__ == '__main__':
+    n = int(input().strip())
+
+    if n % 2 != 0:
+        print("Weird")
+    else:
+        if 2 <= n <= 5:
+            print("Not Weird")
+        elif 6 <= n <= 20:
+            print("Weird")
+        else:
+            print("Not Weird")
