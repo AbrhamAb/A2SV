463c453e9ec4bd2996c6fb3a2cea6f47478c61ab
Abrham Abebaw <abrhamabebaw.tech@gmail.com>
2026-02-09 21:07:38 +0300

    Add solution for checking string equality with one swap
    Implement a solution to check if two strings can be made equal with one swap.

---
+++ 1790._Check_if_One_String_Swap Can_Make_Strings_Equal.py
@@ -0,0 +1,18 @@
+class Solution:
+    def areAlmostEqual(self, s1: str, s2: str) -> bool:
+        diffs = []
+
+        for i in range(len(s1)):
+            if s1[i] != s2[i]:
+                diffs.append(i)
+                if len(diffs) > 2: 
+                    return False
+
+        if len(diffs) == 0:
+            return True  
+
+        if len(diffs) == 2:
+            i, j = diffs
+            return s1[i] == s2[j] and s1[j] == s2[i]
+
+        return False 
