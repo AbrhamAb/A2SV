65dc1430a81707c7e6db61bbaee4e4d26502c94d
Abrham Abebaw <abrhamabebaw.tech@gmail.com>
2026-03-14 22:49:31 +0300

    Add solution for Valid Palindrome (trial 1)

---
+++ leetcode/valid-palindrome_trial1.py
@@ -0,0 +1,15 @@
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        filtered = []
+        for ch in s:
+            if ch.isalnum():
+                filtered.append(ch.lower())
+                
+        left, right = 0, len(filtered) - 1
+        while left < right:
+            if filtered[left] != filtered[right]:
+                return False
+            left += 1
+            right -= 1
+        
+        return True
