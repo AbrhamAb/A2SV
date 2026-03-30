49937ef240547d9aa1ded8f798d6c80aaef46aae
Abrham Abebaw <abrhamabebaw.tech@gmail.com>
2026-03-30 23:45:22 +0300

    Add solution for Valid Palindrome II (trial 1)

--- leetcode/valid-palindrome-ii_trial1.py
+++ leetcode/valid-palindrome-ii_trial1.py
@@ -1,15 +1,20 @@
 class Solution:
     def validPalindrome(self, s: str) -> bool:
         
-        def is_pal(x):
-            return x == x[::-1]
-        
-        if is_pal(s):
+        def is_pal(l, r):
+            while l < r:
+                if s[l] != s[r]:
+                    return False
+                l += 1
+                r -= 1
             return True
 
-        for i in range(len(s)):
-            new_s = s[:i] + s[i+1:]
-            if is_pal(new_s):
-                return True
+        l, r = 0, len(s) - 1
 
-        return False
+        while l < r:
+            if s[l] != s[r]:
+                return is_pal(l + 1, r) or is_pal(l, r - 1)
+            l += 1
+            r -= 1
+
+        return True
