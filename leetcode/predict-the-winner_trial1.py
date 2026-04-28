d17e68154dcaa5045a87d4f1d487ac4dabdfd060
Abrham Abebaw <abrhamabebaw.tech@gmail.com>
2026-04-29 00:12:31 +0300

    Add solution for Predict the Winner (trial 1)

---
+++ leetcode/predict-the-winner_trial1.py
@@ -0,0 +1,20 @@
+class Solution:
+    def predictTheWinner(self, nums: List[int]) -> bool:
+         return len(nums) % 2 == 0 or self.solve(nums)
+    
+    def solve(self, nums):
+        n = len(nums)
+        dp = [[0]*n for _ in range(n)]
+        
+        for i in range(n):
+            dp[i][i] = nums[i]
+        
+        for length in range(2, n+1):
+            for l in range(n - length + 1):
+                r = l + length - 1
+                dp[l][r] = max(
+                    nums[l] - dp[l+1][r],
+                    nums[r] - dp[l][r-1]
+                )
+        
+        return dp[0][n-1] >= 0
